7ef9cfa6c982b570a31c384e4d21ac99deb0d677
Abreham Bekele <97157856+abrish-software@users.noreply.github.com>
2022-09-26 23:48:59 +0300

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def print_list_integer(my_list=[]):
-    for i in my_list
-        print("{:d}".format(i))
+    if my_list:
+        print('\n'.join(['{:d}'.format(n) for n in my_list]))
