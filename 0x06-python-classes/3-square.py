372e9788a95251db130e767f5197eb32735a7a9c
Abreham Bekele <97157856+abrish-software@users.noreply.github.com>
2022-10-11 22:40:09 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-"""Square module - assigns size of the square and
-checks for type and value"""
+"""
+Module 3-square
+Defines class Square with private attribute size and public attribute area
+"""
 
 
 class Square:
-    """defines a square with private instance attribute size"""
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size)
+        area(self)
+    """
+
     def __init__(self, size=0):
-        """assigns size of the square and checks for type and value"""
-        
-        if not isinstance(size, int):
+        """
+        Initializes square
+        Attributes:
+            __size (int): size of a side of square, defaults to 0 if none
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = int(size)
+        else:
+            self.__size = size
 
     def area(self):
-        """public instance method returns current sqr area"""
-        return self.__size ** 2
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
