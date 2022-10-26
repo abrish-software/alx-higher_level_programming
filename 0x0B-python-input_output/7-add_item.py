a0426243294afea21aab60e5597c71088b61d5cb
Abreham Bekele <97157856+abrish-software@users.noreply.github.com>
2022-10-26 11:00:46 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Module for a script that adds all arguments to a Python list"""
-import json
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+'''task 7 module'''
+
+
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+arglist = list(sys.argv[1:])
 
-file_name = "add_item.json"
-
-# can also use os.path.exists(file) for try and except
-# be sure to import os.path
 try:
-    json_list = load_from_json_file(file_name)
-except:
-    json_list = []
-for item in argv[1:]:
-    json_list.append(item)
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
 
-save_to_json_file(json_list, file_name)
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
